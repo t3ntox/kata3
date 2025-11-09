b66a9da9fe2d6aeed19928781accc9c3baed21db
vidal <vidal.de101@alu.ulpgc.es>
2025-11-09 13:22:29 +0000

    Main Changed

--- src/main/java/software/ulpgc/is2/kata3/application/Main.java
+++ src/main/java/software/ulpgc/is2/kata3/application/Main.java
@@ -1,14 +1,35 @@
 package software.ulpgc.is2.kata3.application;
 
 import software.ulpgc.is2.kata3.model.Movie;
+import software.ulpgc.is2.kata3.tasks.HistogramBuilder;
+import software.ulpgc.is2.kata3.viewmodel.Histogram;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        List<Movie> movieList = new RemoteMovieLoader().loadAll();
-        for (Movie movie: movieList) {
-            System.out.println(movie);
+        List<Movie> movieList = new RemoteMovieLoader(Main::fromTsv).loadAll();
+        Histogram histogram = new HistogramBuilder(movieList).build(Movie::year);
+        for (int bin: histogram) {
+            System.out.println(bin + ": " + histogram.count(bin));
         }
+    }
+
+    private static Movie fromTsv(String line) {
+        return fromTsv(line.split("\t"));
+    }
+
+    private static Movie fromTsv(String[] split) {
+        return new Movie(
+                split[2],
+                toInt(split[5]),
+                toInt(split[7])
+        );
+    }
+
+    private static int toInt(String s) {
+        if (s.equals("\\N")) return -1;
+        return Integer.parseInt(s);
     }
 }
--- src/main/java/software/ulpgc/is2/kata3/viewmodel/Histogram.java
+++ src/main/java/software/ulpgc/is2/kata3/viewmodel/Histogram.java
@@ -15,7 +15,7 @@ public class Histogram implements Iterable<Integer>{
         this.map.put(bin, count(bin)+1);
     }
 
-    private int count(int bin) {
+    public int count(int bin) {
         return this.map.getOrDefault(bin,0);
     }
 
